daaae0b7b33a3ae12ebccce5aff6c47317b66187
AsusTp500 <AsusTp500@ASUS>
2018-03-23 18:32:41 +0700

    Add project files.

---
+++ Rock-Paper-Scissor/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rock_Paper_Scissor
+{
+    public partial class Form1 : Form
+    {
+        byte[] data = new byte[1024];
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+        Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        IPEndPoint sender = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+        EndPoint remote;
+        string input;
+        public Form1()
+        {
+            InitializeComponent();
+            server.SendTo(data, data.Length, SocketFlags.None, ipep);
+            remote = (EndPoint)sender;
+            data = new byte[1024];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Kéo";
+            input = "2";
+            server.SendTo(Encoding.ASCII.GetBytes(input), remote);
+            data = new byte[1024];
+            int recv = server.ReceiveFrom(data, ref remote);
+            string kq = Encoding.ASCII.GetString(data, 0, recv);
+            KetQua(input, kq);
+            System.Threading.Thread.Sleep(5000);
+            DialogResult time =MessageBox.Show("Ban co muon choi tiep ko?","Replay", MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+            if(time==DialogResult.Yes)
+            {
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+            }
+            else
+            {
+                server.Close();
+                this.Close();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Bao";
+            input = "3";
+            server.SendTo(Encoding.ASCII.GetBytes(input), remote);
+            data = new byte[1024];
+            int recv = server.ReceiveFrom(data, ref remote);
+            string kq = Encoding.ASCII.GetString(data, 0, recv);
+            KetQua(input, kq);
+            System.Threading.Thread.Sleep(5000);
+            DialogResult time = MessageBox.Show("Ban co muon choi tiep ko?", "Replay", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (time == DialogResult.Yes)
+            {
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+            }
+            else
+            {
+                server.Close();
+                this.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Búa";
+            input = "1";
+            server.SendTo(Encoding.ASCII.GetBytes(input), remote);
+            data = new byte[1024];
+            int recv = server.ReceiveFrom(data, ref remote);
+            string kq = Encoding.ASCII.GetString(data, 0, recv);
+            KetQua(input, kq);
+            System.Threading.Thread.Sleep(5000);
+            DialogResult time = MessageBox.Show("Ban co muon choi tiep ko?", "Replay", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (time == DialogResult.Yes)
+            {
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+            }
+            else
+            {
+                server.Close();
+                this.Close();
+            }
+        }
+        public void KetQua(string input, string k)
+        {
+            if (input == "1")
+            {
+                if (k == "1")
+                {
+                    textBox3.Text = "Búa";
+                    textBox2.Text = "Hòa";
+                }
+                if (k == "2")
+                {
+                    textBox3.Text = "Kéo";
+                    textBox2.Text = "Thắng";
+                }
+                if (k == "3")
+                {
+                    textBox3.Text = "Bao";
+                    textBox2.Text = "Thua";
+                }
+            }
+            if (input == "2")
+            {
+                if (k == "1")
+                {
+                    textBox3.Text = "Búa";
+                    textBox2.Text = "Thua";
+                }
+                if (k == "2")
+                {
+                    textBox3.Text = "Kéo";
+                    textBox2.Text = "Hòa";
+                }
+                if (k == "3")
+                {
+                    textBox3.Text = "Bao";
+                    textBox2.Text = "Thắng";
+                }
+            }
+            if (input == "3")
+            {
+                if (k == "1")
+                {
+                    textBox3.Text = "Búa";
+                    textBox2.Text = "Thắng";
+                }
+                if (k == "2")
+                {
+                    textBox3.Text = "Kéo";
+                    textBox2.Text = "Thua";
+                }
+                if (k == "3")
+                {
+                    textBox3.Text = "Bao";
+                    textBox2.Text = "Hòa";
+                }
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+            int recv;
+            byte[] data = new byte[1024];
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+            Socket newsock = new Socket(AddressFamily.InterNetwork,SocketType.Dgram,ProtocolType.Udp);
+            newsock.Bind(ipep);
+            Console.WriteLine("Waiting for a client...");
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint remote = (EndPoint)(sender);
+            recv = newsock.ReceiveFrom(data, ref remote);
+            Console.WriteLine("Message received from {0}:", remote.ToString());
+            while(true)
+            {
+                data = new byte[1024];
+                recv = newsock.ReceiveFrom(data, ref remote);
+                string kq = Encoding.ASCII.GetString(data, 0, recv);
+                if (kq == "1") Console.WriteLine("{0} chon bua", remote.ToString());
+                if (kq == "2") Console.WriteLine("{0} chon keo", remote.ToString());
+                if (kq == "3") Console.WriteLine("{0} chon bao", remote.ToString());
+                int n = rd.Next(1, 4);
+                if (n == 1) Console.WriteLine("Ban chon Bua");
+                if (n == 2) Console.WriteLine("Ban chon Keo");
+                if (n == 3) Console.WriteLine("Ban chon Bao");
+                newsock.SendTo(Encoding.ASCII.GetBytes(n.ToString()), recv, SocketFlags.None, remote);
+            }
+        }
+    }
+}
